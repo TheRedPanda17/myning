63bc16f1434b1cf064e525b6c61084639258829b
TheRedPanda17 <calebandrewrobinson@gmail.com>
2023-08-09 18:29:43 -0500

    Add stats singleton

--- myning/migrations/__init__.py
+++ myning/migrations/__init__.py
@@ -1,4 +1,11 @@
-from . import garden_transfer, macfuffin_transfer, research_transfer, soul_credits, species_pokedex
+from . import (
+    garden_transfer,
+    macfuffin_transfer,
+    research_transfer,
+    soul_credits,
+    species_pokedex,
+    stats_transfer,
+)
 
 MIGRATIONS = {
     1: species_pokedex,
@@ -6,4 +13,5 @@ MIGRATIONS = {
     3: garden_transfer,
     4: research_transfer,
     5: macfuffin_transfer,
+    6: stats_transfer,
 }
