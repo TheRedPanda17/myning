483b3051fd482847b6c24d133e58f79dbfd20602
rj-lee <rjlee1910@gmail.com>
2023-08-28 19:06:49 -0500

    Remove unnecessary widget updates
    The first pick in ChapterWidget's on_mount will call update_dashboard

--- myning/tui/inventory.py
+++ myning/tui/inventory.py
@@ -15,7 +15,6 @@ class InventoryWidget(DataTable):
         self.add_column("i")
         self.add_column("n", width=32)
         self.add_column("v")
-        self.update()
 
     def on_click(self):
         self.focus()
