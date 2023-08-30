c046ad07a82c2c9577488e6f7a32c25e049fe242
Caleb Robinson <31993348+TheRedPanda17@users.noreply.github.com>
2023-08-29 22:09:13 -0500

    Show exact species stats (#63)
    * Show exact species stats
    
    * Add adjectives back

--- myning/chapters/journal.py
+++ myning/chapters/journal.py
@@ -3,12 +3,14 @@ from functools import partial
 from rich.table import Table
 
 from myning.chapters import Option, PickArgs, main_menu
-from myning.config import SPECIES
+from myning.config import RESEARCH, SPECIES
 from myning.objects.player import Player
+from myning.objects.research_facility import ResearchFacility
 from myning.objects.species import Species
 from myning.utilities.ui import Colors, Icons
 
 player = Player()
+facility = ResearchFacility()
 
 
 def enter():
@@ -37,12 +39,18 @@ def show(species: Species):
             message="You have not discovered this species yet.",
             options=[Option("Go Back", enter)],
         )
+
+    exact_stats = (
+        facility.has_research("show_species_stats")
+        and RESEARCH["show_species_stats"].player_value >= species.rarity_tier
+    )
+
     table = Table.grid(padding=(0, 2))
     table.title = f"{species.icon} {species.name}"
     table.title_style = "bold"
     table.add_row()
     table.add_row("Rarity", species.rarity)
-    table.add_row("Stats", species.stats)
+    table.add_row("Stats", species.get_stats(exact_stats))
     table.add_row("Alignment", species.alignment)
     return PickArgs(
         message=table,
--- myning/objects/species.py
+++ myning/objects/species.py
@@ -9,13 +9,21 @@ SKILL_GAPS = {
     "critical_chance": [0, 1, 3, 5, 7],
     "dodge_chance": [0, 1, 3, 5, 7],
 }
+SKILL_COLORS = [
+    "[red1]{msg}[/]",
+    "[orange1]{msg}[/]",
+    "[grey70]{msg}[/]",
+    "[cyan1]{msg}[/]",
+    "[dodger_blue1]{msg}[/]",
+    "[purple]{msg}[/]",
+]
 ADJECTIVES = [
-    "[red1]Very Low[/]",
-    "[orange1]Low[/]",
-    "[grey70]Average[/]",
-    "[cyan1]High[/]",
-    "[dodger_blue1]Very High[/]",
-    "[purple]Unbeatable[/]",
+    "Very Low",
+    "Low",
+    "Average",
+    "High",
+    "Very High",
+    "Unbeatable",
 ]
 RARITIES = [
     "[grey70]Very Common[/]",
@@ -63,22 +71,29 @@ class Species(Object):
     def from_dict(cls, attrs: dict):
         return cls(**attrs)
 
-    def get_skill_adj(self, skill: str):
+    def get_skill_adj(self, skill: str, exact: bool):
         level = getattr(self, skill)
         gaps = SKILL_GAPS[skill]
-        return next(
-            (ADJECTIVES[index] for index, gap_level in enumerate(gaps) if level <= gap_level),
-            ADJECTIVES[-1],
-        )
 
-    @property
-    def stats(self):
+        index = -1
+        for i, gap_level in enumerate(gaps):
+            if level <= gap_level:
+                index = i
+                break
+
+        msg = ADJECTIVES[index]
+        if exact:
+            msg = f"{level} - {msg}"
+
+        return SKILL_COLORS[index].format(msg=msg)
+
+    def get_stats(self, exact: bool):
         table = Table.grid(padding=(0, 1))
         table.add_row("Base Health", Icons.HEART, f"[green1]{self.health_mod}[/]")
-        table.add_row("Strength", Icons.WEAPON, self.get_skill_adj("strength"))
-        table.add_row("Defense", Icons.ARMOR, self.get_skill_adj("defense"))
-        table.add_row("Critical chance", Icons.CRIT, self.get_skill_adj("critical_chance"))
-        table.add_row("Dodge chance", Icons.DODGE, self.get_skill_adj("dodge_chance"))
+        table.add_row("Strength", Icons.WEAPON, self.get_skill_adj("strength", exact))
+        table.add_row("Defense", Icons.ARMOR, self.get_skill_adj("defense", exact))
+        table.add_row("Critical chance", Icons.CRIT, self.get_skill_adj("critical_chance", exact))
+        table.add_row("Dodge chance", Icons.DODGE, self.get_skill_adj("dodge_chance", exact))
         return table
 
     @property
